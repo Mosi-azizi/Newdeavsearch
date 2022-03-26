0230a7423f8f6ecee9ad8be13d00ced0e92dccbf
SAM <85979367+Mosi-azizi@users.noreply.github.com>
2022-03-26 16:17:37 +0430

    edit profile

--- projects/views.py
+++ projects/views.py
@@ -32,12 +32,17 @@ def project(request, pk):
 
 @login_required(login_url='users:login')
 def createProject(request):
+    profile = request.user.profile
     form = ProjectForm()
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
             # print(request.POST)
-            form.save()
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+
+
             return redirect('projects:projects')
     context = {
         'form':form
@@ -47,7 +52,8 @@ def createProject(request):
 
 @login_required(login_url='users:login')
 def updateProject(request,pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES, instance=project)
@@ -62,7 +68,8 @@ def updateProject(request,pk):
 
 @login_required(login_url='users:login')
 def deleteProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     if request.method == 'POST':
         project.delete()
         return redirect('projects:projects')
